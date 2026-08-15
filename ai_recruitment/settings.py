c8194d8f2cce8a2e7237d795fd632a2be2196b37
YasasmiUdugama <yasasmineranjana00@gmail.com>
2026-08-15 19:46:16 +0530

    First

--- ai_recruitment/settings.py
+++ ai_recruitment/settings.py
@@ -101,8 +101,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'hiran11385@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'dwrp ferg gymk hyyg' \
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'hr2026interview@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'drcxz jgum cire pbtc' \
 '')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
